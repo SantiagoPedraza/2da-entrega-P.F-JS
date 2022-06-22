ba69d090bf72ae0058e222696ca30f03a14fa896
santiago pedraza <103968703+SantiagoPedraza@users.noreply.github.com>
2022-06-22 18:58:05 -0300

    Initial commit
    2da entrega proyecto final

---
+++ JS/stock.js
@@ -0,0 +1,15 @@
+let stockProductos = [
+    {id: 1, nombre: "Chaleco Champions ", tipo: "chaleco", desc: "¿que esperas para comparlo?", precio: 12000, talle: "L", img: './img/1.jpg'},
+    {id: 2, nombre: "Hoodie Nike", tipo: "buzo", desc: "¿que esperas para comparlo?", precio: 8000, talle: "XL", img: './img/2.jpg'},
+    {id: 3, nombre: "Buzo Swoosh Nike", tipo: "buzo", desc: "¿que esperas para comparlo?", precio: 9000, talle: "M", img: './img/3.jpg'},
+    {id: 4, nombre: "Sweater Penguin", tipo: "sweater", desc: "¿que esperas para comparlo?", precio: 7000, talle: "S", img: './img/4.jpg'},
+    {id: 5, nombre: "Chomba Lacoste", tipo: "chomba", desc: "¿que esperas para comparlo?", precio: 10000, talle: "XXL", img: './img/5.jpg'},
+    {id: 6, nombre: "Remera terminca Under Armor", tipo: "remera", desc: "¿que esperas para comparlo?", precio: 8500, talle: "S", img: './img/6.jpg'},
+    {id: 7, nombre: "Joggin Cargo Nike", tipo: "Joggin", desc: "¿que esperas para comparlo?", precio: 5000, talle: "L", img: './img/7.jpg'},
+    {id: 8, nombre: "Campera The Nort Face", tipo: "campera", desc: "¿que esperas para comparlo?", precio: 15000, talle: "XL", img: './img/8.jpg'},
+    {id: 9, nombre: "Campera The Nort Face de pluma", tipo: "campera", desc: "¿que esperas para comparlo?", precio: 20000, talle: "L", img: './img/9.jpg'},
+    {id: 10, nombre: "Chupin Algodon Nike", tipo: "chupin", desc: "¿que esperas para comparlo?", precio: 4000, talle: "M", img: './img/10.jpg'},
+    {id: 10, nombre: "Campera Champions", tipo: "campera", desc: "¿que esperas para comparlo?", precio: 14000, talle: "xl", img: './img/11.jpg'},
+    {id: 10, nombre: "Buzo The Nort Face", tipo: "buzo", desc: "¿que esperas para comparlo?", precio: 7000, talle: "M", img: './img/12.jpg'},
+   
+]
