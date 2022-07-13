0a7b30f4732070cf768e985cbd8d012275cf9ec9
santiago pedraza <103968703+SantiagoPedraza@users.noreply.github.com>
2022-07-13 20:23:59 -0300

    .
    FETCH

--- JS/app.js
+++ JS/app.js
@@ -6,32 +6,57 @@ const precioTotal = document.querySelector('#precioTotal')
 
 const btnVaciar = document.getElementById('vaciarCarrito')
 
-let carrito
-const carritoEnLS = JSON.parse( localStorage.getItem('carrito') )
+
+const carrito = JSON.parse(localStorage.getItem('carrito')) || []
+
+let stock = []
+
+
+fetch('JS/stock.json')
+    .then((resp) => resp.json())
+    .then((data) => {
+        stock = data
+            
+        stock.forEach((producto) => {
+            const div = document.createElement('div')
+            div.classList.add('producto')
+        
+            div.innerHTML = `
+                            <img src=${producto.img} alt="">
+                            <h3>${producto.nombre}</h3>
+                            <p>${producto.desc}</p>
+                            <p>Talle: ${producto.talle}</p>
+                            ${producto.freeshipping === true ? '<p><strong>Envío gratis</strong></p>' : ''}
+                            <p class="precioProducto">Precio: $${producto.precio}</p>
+                            <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+                        `
+        
+            productosContainer.append(div)
+        })
+    })
 
 
 
-stockProductos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add('producto')
 
-    div.innerHTML = `
-                    <img src=${producto.img} alt="">
-                    <h3>${producto.nombre}</h3>
-                    <p>${producto.desc}</p>
-                    <p>Talle: ${producto.talle}</p>
-                    <p class="precioProducto">Precio: $${producto.precio}</p>
-                    <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
-                `
+const agregarAlCarrito = (productId) => {
+    
+    const itemInCart = carrito.find((producto) => producto.id === productId)
 
-    productosContainer.append(div)
-})
-
-
-
-const agregarAlCarrito = (id) => {
-    const item = stockProductos.find( (producto) => producto.id === id)
-    carrito.push(item)
+    if (itemInCart) {
+        itemInCart.cantidad += 1
+        showMensaje(itemInCart.nombre)
+    } else {
+        const {id, nombre, precio} = stock.find( (producto) => producto.id === productId)
+    
+        const itemToCart = {
+            id, 
+            nombre, 
+            precio, 
+            cantidad: 1
+        }
+        carrito.push(itemToCart)
+        showMensaje(nombre)
+    }
 
     localStorage.setItem('carrito', JSON.stringify(carrito))
 
@@ -43,8 +68,24 @@ const agregarAlCarrito = (id) => {
 
 const removerDelCarrito = (id) => {
     const item = carrito.find((producto) => producto.id === id)
-    const indice = carrito.indexOf(item)
-    carrito.splice(indice, 1)
+
+    item.cantidad -= 1
+    
+
+    if (item.cantidad === 0) {
+        const indice = carrito.indexOf(item)
+        carrito.splice(indice, 1)
+    }
+
+    Toastify({
+        text: `Se eliminó 1 unidad de ${item.nombre}`,
+        position: 'left',
+        gravity: 'bottom',
+        duration: 5000,
+        style: {
+            background: "linear-gradient(to right, #f17b5d, #f02f2f)",
+          }
+    }).showToast()
 
     localStorage.setItem('carrito', JSON.stringify(carrito))
 
@@ -55,7 +96,6 @@ const removerDelCarrito = (id) => {
 
 const vaciarCarrito = () => {
     carrito.length = 0
-
     localStorage.setItem('carrito', JSON.stringify(carrito))
 
     renderCarrito()
@@ -63,7 +103,33 @@ const vaciarCarrito = () => {
     renderTotal()
 }
 
-btnVaciar.addEventListener('click', vaciarCarrito)
+
+btnVaciar.addEventListener('click', () => {
+    Swal.fire({
+        title: 'Está seguro?',
+        text: "Está a punto de vaciar el carrito",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, vaciar',
+        cancelButtonText: 'No, cancelar'
+      }).then( (result) => {
+            if (result.isConfirmed) {
+                vaciarCarrito()
+                botonCerrar.click()
+                Toastify({
+                    text: 'Se vació el carrito',
+                    position: 'left',
+                    gravity: 'bottom',
+                    duration: 5000,
+                    style: {
+                        background: "linear-gradient(to right, #f17b5d, #f02f2f)",
+                      }
+                }).showToast()
+            }
+      } )
+})
 
 const renderCarrito = () => {
     carritoContenedor.innerHTML = ''
@@ -74,7 +140,8 @@ const renderCarrito = () => {
 
         div.innerHTML = `
                     <p>${item.nombre}</p>
-                    <p>Precio: $${item.precio}</p>
+                    <p>Cantidad: ${item.cantidad}</p>
+                    <p>Precio unitario: $${item.precio}</p>
                     <button onclick="removerDelCarrito(${item.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
                     `
         
@@ -83,25 +150,33 @@ const renderCarrito = () => {
 }
 
 const renderCantidad = () => {
-    contadorCarrito.innerText = carrito.length
+    contadorCarrito.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad, 0)
 }
 
 const renderTotal = () => {
     let total = 0
     carrito.forEach((producto) => {
-        total += producto.precio
+        total += producto.precio * producto.cantidad
     })
 
     precioTotal.innerText = total
 }
 
-
-if (carritoEnLS) {
-    carrito = carritoEnLS
-
-    renderCarrito()
-    renderCantidad()
-    renderTotal()
-} else {
-    carrito = []
+const showMensaje = (nombre) => {
+    Toastify({
+        text: `Se agregó 1 unidad de ${nombre} al carrito!`,
+        duration: 3000,
+        gravity: 'bottom',
+        position: 'left',
+        onClick: () => {
+            botonAbrir.click()
+        },
+        style: {
+            background: "BLUE",
+          }
+    }).showToast()
 }
+
+renderCarrito()
+renderCantidad()
+renderTotal()
